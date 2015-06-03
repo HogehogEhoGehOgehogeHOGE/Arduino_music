74c1b8eda94811d252a3d0851b15176174564ab2
unknown <ac2351yosh@PC322.ed.y.kumamoto-nct.ac.jp>
2015-06-03 10:46:43 +0900

    絢香という歌手のにじいろという曲の冒頭部分が入っています

---
+++ sketch_raimbow/sketch_raimbow.ino
@@ -0,0 +1,50 @@
+const int loudspeaker_pin = 13;
+const int base_time = 16 * 50;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(13, HIGH);
+  delay(10);
+  digitalWrite(13, LOW);
+  delay(100);
+tone(loudspeaker_pin, 880);
+delay(base_time / 2);
+tone(loudspeaker_pin, 831);
+delay(base_time / 2);
+tone(loudspeaker_pin, 880);
+delay(base_time / 2);
+tone(loudspeaker_pin, 988);
+delay(base_time / 2);
+tone(loudspeaker_pin, 1047);
+delay(base_time * 3 / 2 / 2);
+tone(loudspeaker_pin, 988);
+delay(base_time / 2);
+tone(loudspeaker_pin, 880);
+delay(base_time / 2);
+tone(loudspeaker_pin, 831);
+delay(base_time / 2);
+tone(loudspeaker_pin, 698);
+delay(base_time / 2);
+tone(loudspeaker_pin, 659);
+delay(base_time / 2);
+tone(loudspeaker_pin, 698);
+delay(base_time / 2);
+tone(loudspeaker_pin, 784);
+delay(base_time / 2);
+tone(loudspeaker_pin, 880);
+delay(base_time * 3 / 2 / 2);
+tone(loudspeaker_pin, 784);
+delay(base_time / 2);
+tone(loudspeaker_pin, 659);
+delay(base_time / 2);
+tone(loudspeaker_pin, 587);
+delay(base_time / 2);
+
+
+}
+
